ba07d6110ff927b45834bcf160bdcd94c2f10607
onk <takafumi.onaka@gmail.com>
2014-12-01 15:47:59 +0900

    mount minimal rack app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount proc {|env| [200, {}, ["Hello, World!"]] }, at: "hello"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
